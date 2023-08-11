52a695cd6faadff1e41ac31ee60ac9047cf8979f
Caio Bello Zamana <caiobzm@gmail.com>
2023-08-11 01:02:00 -0300

    Initial Commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,9 +4,8 @@ import './style.css';
 const Header = () => {
   return (
       <header className="header">
-        <h1>Gitfind</h1>
-        <p>Bem-vindo à minha aplicação React!</p>
-        <p>Consulte o perfil de algum usuário do GitHub para verificar a lista de seu repositório</p>
+        <h1>Welcome to Gitfind</h1>
+        <p>Consulte o perfil de algum usuário do GitHub para verificar a lista de seu repositório.</p>
 
       </header>
 
